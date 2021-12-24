355258f1043e4aea1e77918400eb5604d8857abc
cokejoke <cokejoke@users.noreply.github.com>
2021-12-24 03:47:38 +0000

    deploy: 926b83935270c7e003dedda8fc7ec71b9f8e237b

---
+++ _nuxt/static/1640317657/admin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Freview-ui\u002F_nuxt\u002Fstatic\u002F1640317657",layout:"default",error:a,serverRendered:b,routePath:"\u002Fadmin",config:{_app:{basePath:"\u002Freview-ui\u002F",assetsPath:"\u002Freview-ui\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,""));
