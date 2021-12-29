3996bdc14056b97a946a88474944a794b7c5421c
cokejoke <justin@knoter.eu>
2021-12-29 16:14:29 +0100

    :sparkles: Add Input

---
+++ components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <input :style="'background: ' + bg" :type="type" :placeholder="placeholder" />
+</template>
+
+<script lang="ts">
+export default {
+  props: ["type", "placeholder", "bg"],
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+  box-sizing: border-box;
+  box-shadow: none;
+  border: none;
+  padding: 10px;
+  font-size: 18px;
+  width: 100%;
+  background: var(--bg-secondary);
+  color: var(--color-secondary);
+}
+</style>
